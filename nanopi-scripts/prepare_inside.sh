4f7d94d6e1b9ac39cf3a463d10ebc6a1df09ab09
Edmunt Pienkowsky <roed@onet.eu>
2016-02-18 14:58:52 +0000

    journald.conf not updated

--- nanopi-scripts/prepare_inside.sh
+++ nanopi-scripts/prepare_inside.sh
@@ -8,9 +8,9 @@ echo -e "y\ny\n" | pacman -Scc
 echo '[nanopi] Updating configuration files'
 [ -f /etc/dhcpd.conf.pacnew ] && mv /etc/dhcpd.conf.pacnew /etc/dhcpd.conf
 [ -f /etc/fstab.pacnew ] && mv /etc/fstab.pacnew /etc/fstab
-[ -f /etc/dhcpd.conf.pacnew ] && mv /etc/dhcpd.conf.pacnew /etc/dhcpd.conf
+[ -f /etc/systemd/journald.conf.pacnew ] && mv /etc/systemd/journald.conf.pacnew /etc/systemd/journald.conf
 
-echo '[nanopi] Rebuilding man database'
+echo '[nanopi] Building man database'
 mandb -q
 
 echo '[nanopi] Bye from inside'
