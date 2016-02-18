247c82086ee028afb84b77bfa308ccc5a81c945f
Edmunt Pienkowsky <roed@onet.eu>
2016-02-18 15:34:21 +0100

    Updating configuration files

---
+++ prepare_inside.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pacman -U /root/custompkg/linux-armv5-nanopi-*-arm.pkg.tar.xz /root/custompkg/nanopi-config-*-arm.pkg.tar.xz /root/custompkg/nanopi-nanoboot-*-arm.pkg.tar.xz --force --noconfirm --needed
+
+echo '[nanopi] Clearing pacgage cache'
+echo -e "y\ny\n" | pacman -Scc
+
+echo '[nanopi] Updating configuration files'
+[ -f /etc/fstab.pacnew ] || mv /etc/fstab.pacnew /etc/fstab
+[ -f /etc/dhcpd.conf.pacnew ] || mv /etc/dhcpd.conf.pacnew /etc/dhcpd.conf
+
+echo '[nanopi] Rebuilding man database'
+mandb -q
+
+echo '[nanopi] Bye from inside'
