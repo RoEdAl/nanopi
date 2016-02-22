99b19fe43aa46de3fd1fbc0873cc1b5270071810
Edmunt Pienkowsky <roed@onet.eu>
2016-02-22 03:46:12 +0000

    journald: separate config file

--- nanopi-scripts/prepare_inside.sh
+++ nanopi-scripts/prepare_inside.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo '[nanopi] Hello from inside'
-hostnamectl set-hostname nanopi
-hostnamectl set-hassis embedded
+# hostnamectl set-hostname nanopi
+# hostnamectl set-hassis embedded
 
 echo '[nanopi] Installing additional packages'
 echo -e "n\n" | pacman -U /root/custompkg/*-arm.pkg.tar.xz --force --noconfirm --needed
@@ -16,7 +16,12 @@ echo '[nanopi] Updating configuration files'
     mv /etc/dhcpd.conf.pacnew /etc/dhcpd.conf
 }
 [ -f /etc/fstab.pacnew ] && mv /etc/fstab.pacnew /etc/fstab
-[ -f /etc/systemd/journald.conf.pacnew ] && mv /etc/systemd/journald.conf.pacnew /etc/systemd/journald.conf
+
+echo '[nanopi] Removing /var/log/journal'
+[ -d /var/log/journal ] && rm -rf /var/log/journal
+
+echo '[nanopi] Removing /etc/sysctl.d/100-manjaro.conf'
+[ -f /etc/sysctl.d/100-manjaro.conf ] && rm /etc/sysctl.d/100-manjaro.conf
 
 echo '[nanopi] Building man database'
 mandb -q
